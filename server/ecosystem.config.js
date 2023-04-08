937ac811fbe5fc8fcd87c3d7d729705710a886a0
Mahan Esmaeilzadeh <mesmaeilzadeh@bcgsc.ca>
2023-04-08 15:47:24 -0700

    added pm2

---
+++ server/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [
+      {
+        name: 'appServer',
+        script: './appServer.js',
+        watch: false,
+        exec_mode: 'fork',
+      },
+      {
+        name: 'authServer',
+        script: './authServer.js',
+        watch: false,
+        exec_mode: 'fork',
+      },
+    ],
+  };
+  
